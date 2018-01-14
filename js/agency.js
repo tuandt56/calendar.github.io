4a429cc58813a9fb4e5ea96d91f9b68b6c61e3ba
tuandt56 <34866119+tuandt56@users.noreply.github.com>
2018-01-14 21:53:27 +0700

    xhZBVkzBV

--- js/agency.js
+++ js/agency.js
@@ -14,6 +14,16 @@
       }
     }
   });
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106890057-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-106890057-3');
+</script>
+
   //Hide key information
   $(document).ready(function () {
     // $('.key').hide();
